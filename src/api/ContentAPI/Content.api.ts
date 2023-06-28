0bf6c69f59b9946d207c15f684adb9ebcbba2b2a
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-06-28 22:55:01 +0200

    Create api, model, mocks

---
+++ src/api/ContentAPI/Content.api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import { BASE_URL, useAPImocks } from '../config';
+import { ContentDTO } from './Content.dto';
+import { getContentMock } from './Content.mock';
+
+export const getContent = () =>
+  useAPImocks
+    ? getContentMock()
+    : axios
+        .get<ContentDTO>(
+          `${BASE_URL}/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/db4930e9-7504-4d9d-ae6c-33facca754d1`
+        )
+        .then(({ data }) => data);
--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,3 @@
 export const useAPImocks = false;
+
+export const BASE_URL = `https://content-eu-4.content-cms.com`;
